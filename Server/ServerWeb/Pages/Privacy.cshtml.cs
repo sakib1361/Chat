90d9a236a6f8d9563b8e325dafc3b2e2a78aa47c
Shadman Sakib <sakib.buet51@outlook.com>
2019-07-06 10:39:40 +0600

    Web Init

---
+++ Server/ServerWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServerWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
